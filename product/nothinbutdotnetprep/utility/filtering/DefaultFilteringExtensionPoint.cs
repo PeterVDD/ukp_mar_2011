205aeec3afacd3e3eed86dd7a94eff5924c7631b
Peter Van den Daele <peter.vandendaele@gmail.com>
2011-03-15 16:14:39 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/DefaultFilteringExtensionPoint.cs
+++ product/nothinbutdotnetprep/utility/filtering/DefaultFilteringExtensionPoint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetprep.utility.filtering
 {
@@ -22,6 +23,49 @@ namespace nothinbutdotnetprep.utility.filtering
         {
             return new PropertyCriteria<ItemToFilter, ReturnType>(property_accessor,
                                                                           property_criteria);
+        }
+    }
+
+    public class DefaultIEnumerableExtensionPoint<ItemToFilter, ReturnType> : IEnumerableExtensionPoint<ItemToFilter, ReturnType>
+    {
+        PropertyAccessor<ItemToFilter, ReturnType> property_accessor;
+        private System.Collections.Generic.IEnumerable<ItemToFilter> items;
+
+        public DefaultIEnumerableExtensionPoint(IEnumerable<ItemToFilter> items, PropertyAccessor<ItemToFilter, ReturnType> property_accessor)
+        {
+            this.items = items;
+            this.property_accessor = property_accessor;
+        }
+
+        public IEnumerableExtensionPoint<ItemToFilter, ReturnType> not
+        {
+            get
+            {
+                return new NegatingIEnumerableExtensionPoint<ItemToFilter, ReturnType>(this);
+            }
+        }
+
+        public IEnumerable<ItemToFilter> execute(Criteria<ReturnType> property_criteria)
+        {
+
+            var criteria = new PropertyCriteria<ItemToFilter, ReturnType>(property_accessor,
+                                                                         property_criteria);
+            return items.all_items_matching(criteria);
+        }
+    }
+
+    public class NegatingIEnumerableExtensionPoint<ItemToFilter, ReturnType>:IEnumerableExtensionPoint<ItemToFilter, ReturnType>
+    {
+        private IEnumerableExtensionPoint<ItemToFilter, ReturnType> original;
+
+        public NegatingIEnumerableExtensionPoint(IEnumerableExtensionPoint<ItemToFilter, ReturnType> enumerableExtensionPoint)
+        {
+            this.original = enumerableExtensionPoint;
+        }
+
+        public IEnumerable<ItemToFilter> execute(Criteria<ReturnType> property_criteria)
+        {
+            return original.execute(property_criteria.not());
         }
     }
 }
